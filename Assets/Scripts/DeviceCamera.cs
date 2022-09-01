b544a3e103e8b1b04cc1a2cb3c802b0e95172b5d
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-08-31 21:02:45 -0700

    improvements to costume view, sizing, and layout.

--- Assets/Scripts/DeviceCamera.cs
+++ Assets/Scripts/DeviceCamera.cs
@@ -9,17 +9,29 @@ public class DeviceCamera : MonoBehaviour
     private bool cameraAvailable;
     private WebCamTexture backCamera;
     private Texture defaultBackground;
-
     private Texture capturedTexture;
+    Texture2D pipTexture;
 
+    [SerializeField]
+    GameObject CameraCanvas;
+    [SerializeField]
+    GameObject CameraPanel;
+    [SerializeField]
+    GameObject CostumePanel;
     [SerializeField]
     RawImage background;
     [SerializeField]
     RawImage picInPic;
     [SerializeField]
+    RawImage suitFace;
+    [SerializeField]
     AspectRatioFitter fit;
     [SerializeField]
     TextMeshProUGUI DebugText;
+    [SerializeField]
+    RectTransform FramingWindow;
+
+    float captureSquareLength = 200f;
 
     // Start is called before the first frame update
     void Start()
@@ -62,6 +74,7 @@ public class DeviceCamera : MonoBehaviour
         float ratio = (float)backCamera.width / (float)backCamera.height;
 
         background.rectTransform.sizeDelta = new Vector2((float)backCamera.width, (float)backCamera.height);
+        FramingWindow.sizeDelta = new Vector2(captureSquareLength, captureSquareLength);
 
         fit.aspectRatio = ratio;
 
@@ -74,7 +87,6 @@ public class DeviceCamera : MonoBehaviour
         DebugText.text = "w:" + backCamera.width + ", h:" + backCamera.height + ", r:" + ratio + ", s:" + scaleY + ", o:" + orient;
     }
 
-    Texture2D pipTexture;
     public void CaptureImage()
     {
         if (!cameraAvailable)
@@ -82,14 +94,31 @@ public class DeviceCamera : MonoBehaviour
 
         float scaleY = backCamera.videoVerticallyMirrored ? -1f : 1f;
         picInPic.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+        suitFace.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         int orient = -backCamera.videoRotationAngle;
         picInPic.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        suitFace.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
-        Color[] c = backCamera.GetPixels((int)(backCamera.width / 2f - 100), (int)(backCamera.height / 2f - 100), 200, 200);
-        pipTexture = new Texture2D(200, 200);
+        Color[] c = backCamera.GetPixels((int)(backCamera.width / 2f - captureSquareLength / 2f), (int)(backCamera.height / 2f - captureSquareLength / 2f), (int)captureSquareLength, (int)captureSquareLength);
+        pipTexture = new Texture2D((int)captureSquareLength, (int)captureSquareLength);
         pipTexture.SetPixels(c);
         pipTexture.Apply();
         picInPic.texture = pipTexture;
+        suitFace.texture = pipTexture;
+    }
+
+    public void ShowCamera()
+    {
+        CameraCanvas.SetActive(true);
+        CameraCanvas.SetActive(true);
+        CostumePanel.SetActive(false);
+    }
+
+    public void ShowCostume()
+    {
+        CameraCanvas.SetActive(false);
+        CameraCanvas.SetActive(false);
+        CostumePanel.SetActive(true);
     }
 }
